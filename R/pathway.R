5063fd6bc2a4d92d1fe4edbaf0a53e43d2a0c1c6
Hua Sun <33794673+hsun9@users.noreply.github.com>
2025-09-05 10:24:42 -0400

    Add files via upload

---
+++ R/pathway.R
@@ -0,0 +1,84 @@
+
+
+#' Calculate gene enrichment by ORA method and pathway
+#'
+#' @param df dataframe
+#' @param org_db org db
+#'
+#' @export
+#'
+RunPathwayGroup <- function(
+    df=NULL, 
+    fun='enrichGO',
+    species = "Homo sapiens",
+    category = "C2",
+    org_db=NULL,
+    ont='ALL',
+    min_n=3,
+    max_n=500,
+    prefix='pathway_group',
+    outdir='.'
+){
+    
+    go_enrich <- NULL
+
+    if (fun=='enrichGO'){
+        go_enrich <- clusterProfiler::compareCluster(
+                        gene~group, 
+                        data=df, 
+                        fun='enrichGO', 
+                        OrgDb=org_db, 
+                        keyType = 'SYMBOL', 
+                        ont = ont, 
+                        readable = T, 
+                        pvalueCutoff = 0.01, 
+                        minGSSize = 5, 
+                        maxGSSize = 500)
+    }
+
+    if (fun=='enricher'){
+        g_msigdb <- msigdbr::msigdbr(species = species, category = category) %>% dplyr::select(gs_name, gene_symbol)
+        go_enrich <- clusterProfiler::compareCluster(
+                        gene~group, 
+                        data=df, 
+                        fun='enricher', 
+                        TERM2GENE=g_msigdb,
+                        readable = T, 
+                        pvalueCutoff = 0.01, 
+                        minGSSize = 5, 
+                        maxGSSize = 500)
+    }
+
+    # First letter to upper case
+    firstup <- function(x) {
+        substr(x, 1, 1) <- toupper(substr(x, 1, 1))
+        x
+    }
+    go_enrich@compareClusterResult$Description <- firstup(go_enrich@compareClusterResult$Description)
+    go_enrich@compareClusterResult$p.adjust <- signif(go_enrich@compareClusterResult$p.adjust, digits=3)
+    
+    # 1.output non-redundancy sig. results
+    # ONTOLOGY ID Description GeneRatio BgRatio pvalue p.adjust qvalue geneID Count
+    write.table(go_enrich@compareClusterResult, file=paste0(outdir, "/ora.", prefix, ".xls"), sep = "\t", row.names = F, quote = F)
+
+    # 2.plot
+    d_emap <- enrichplot::pairwise_termsim(go_enrich)
+    p <- enrichplot::emapplot(filter(d_emap, pvalue < 0.05 & Count > 1), cex.params = list(category_label=.8), pie.params=list(pie='Count'))
+    p <- p + scale_fill_manual(values=c('0'='#FFA500', '1'='#4B0082', '2'='#20B2AA'))
+
+
+    pdf(paste0(outdir, "/ora.", prefix, ".pdf"), width = 8, height = 5, useDingbats=FALSE)
+    print(p)
+    dev.off()
+}
+
+
+
+
+
+
+
+
+
+
+
